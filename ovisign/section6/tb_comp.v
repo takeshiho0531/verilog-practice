6fbd7d10e729e3dad0ee3fb6d6fb4f9624728099
Akiho Kawada <kawadatakeshiho@gmail.com>
2023-10-22 09:54:12 +0900

    feat: add comparator_1bit.v and its testbench

---
+++ ovisign/section6/tb_comp.v
@@ -0,0 +1,28 @@
+`timescale 1us/1ns
+module tb_comp(
+    // no inputs here
+);
+    reg a, b;
+    wire smaller, greater, equal;
+
+    // Instantiate the DUT
+    comparatpr_1bit CMP1(
+        .a(a),
+        .b(b),
+        .smaller(smaller),
+        .greater(greater)
+    );
+
+    initial begin
+        $monitor("a=%b, b=%b, smaller=%b, equal=%b, greater=%b",
+        a, b, smaller, equal, greater);
+
+        #1; a=0; b=0;
+        #1; a=1; b=0;
+        #1; a=1; b=0;
+        #1; a=0; b=1;
+        #1; a=0; b=1;
+        #1; a=1; b=1;
+        #1; $stop;
+    end
+endmodule
